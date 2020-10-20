7bec19d508098ea9442c8154d233950be6fb6565
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-10-20 00:27:30 -0400

    starter-board

---
+++ src/Board.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { Player } from './models'
+  import Space from './Space.svelte'
+
+  let moves = [Player.None, Player.None, Player.None, Player.None, Player.None, Player.None, Player.None, Player.None, Player.None];
+  let turn = Player.X
+
+  const handleMove = (space: number, move: Player) => {
+    moves = [...moves.slice(0, space), move, ...moves.slice(space + 1)]
+    turn = move === Player.X ? Player.O : Player.X
+  }
+</script>
+
+<div>
+  {#each moves as value, index }
+    <Space {index} {value} {turn} onSelect={handleMove} />
+  {/each}
+</div>
+
+<style>
+  div {
+    display: grid;
+    grid-template-columns: 100px 100px 100px;
+    grid-template-rows: 100px 100px 100px;
+  }
+</style>
---
+++ src/Space.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { Player } from './models'
+
+  export let turn: Player;
+  export let value: Player;
+  export let index: number;
+  export let onSelect: (idx: number, value: Player) => void;
+
+  const handleClick = () => {
+    onSelect(index, turn);
+  }
+</script>
+
+<button class={value} on:click={handleClick} disabled={value !== Player.None}>
+  {value === Player.None ? '' : value}
+</button>
+
+<style>
+  button {
+    width: 100%;
+    height: 100%;
+    text-align: center;
+  }
+
+  .x {
+    background: black;
+    color: white;
+  }
+
+  .o {
+    background: white;
+    color: black;
+  }
+</style>
